ad5f03dc281f98e456e85f6839f4821190362c62
Edoardo Biraghi <edoardo.biraghi@gmail.com>
2015-12-17 17:53:47 +0100

    Setting timeout

--- assets/js/simple.js
+++ assets/js/simple.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    setTimeout(function(){$(".author h4").append("developer");}, 5000);
+    setTimeout(function(){$(".author h4").append("developer");}, 2000);
 });
 
 setTimeout(function(){
     $(document).ready(function(){
         $(".author h4").append(" and wizard");
     });
-}, 6000);
+}, 3000);
