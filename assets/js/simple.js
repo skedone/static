4ac908dceb5c38de54c29063320ce0e5503c131c
Edoardo Biraghi <edoardo.biraghi@gmail.com>
2015-12-17 17:48:53 +0100

    Adding more..

---
+++ assets/js/simple.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    setTimeout(function(){$(".author h4").append("developer");}, 5000);
+});
+
+setTimeout(function(){
+    $(document).ready(function(){
+        $(".author h4").append(" and wizard");
+    });
+}, 6000);
