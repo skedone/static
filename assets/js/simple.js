8b00232c538ea29b3b6e9515dd6b18cb8047e1bb
Edoardo Biraghi <edoardo.biraghi@gmail.com>
2016-02-01 16:39:31 +0300

    Delete simple.js

--- assets/js/simple.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    setTimeout(function(){$(".author h4").append("developer");}, 2000);
-});
-
-setTimeout(function(){
-    $(document).ready(function(){
-        $(".author h4").append(" and wizard");
-    });
-}, 3000);
